8cf9a45fb8d61e8e99a6d73004a13d8e7a1b94e5
温蟾圆 <1052403874@outlook.com>
2020-09-26 20:18:16 +0800

    feat: 创建主页布局逻辑

--- app/src/main/java/com/example/vocabulary/MainActivity.java
+++ app/src/main/java/com/example/vocabulary/MainActivity.java
@@ -1,66 +1,26 @@
 package com.example.vocabulary;
 
 import android.os.Bundle;
-
-import com.example.vocabulary.module.Word;
-import com.example.vocabulary.util.WordManager;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Toast;
-
-import org.litepal.LitePal;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-//                LitePal.getDatabase();
-                Word newWord = new Word();
-//                newWord.setEnglish("Hello");
-//                newWord.setChineseInterpretation("嗨");
-//                WordManager.updateWord(newWord, 1);
-//                WordManager.addWord(newWord);
-
-                Toast.makeText(MainActivity.this, WordManager.getWordById(1).getId(), Toast.LENGTH_LONG).show();
-            }
-        });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        int h = getWindowManager().getDefaultDisplay().getHeight();
+        int w = getWindowManager().getDefaultDisplay().getWidth();
+        if (h < w) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.first_content, new NoteListFragment())
+                    .replace(R.id.second_content, new ComposeFragment())
+                    .commit();
+        } else {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.first_content, new NoteListFragment())
+                    .commit();
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
